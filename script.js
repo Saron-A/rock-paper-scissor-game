562eacc4eadb49bbd0e7e40a9291bae2fa8478e9
Saron <saronabebe2019@gmail.com>
2025-02-19 14:23:56 -0800

    Add interactivity

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let btns = document.querySelectorAll("button");
+let div = document.querySelector("div");
+
 let humanScore = 0;
 let compScore = 0;
 
@@ -9,46 +12,50 @@ function getComputerChoice() {
   else return "scissor";
 }
 
-function getHumanChoice() {
-  let me = prompt("Enter your choice: Rock, Paper or Scissor? ");
-  return me;
-}
+// logic to play a single round
+function playRound(humanchoice, compChoice) {
+  let someString;
 
-// logic to play the whole game
-function playGame() {
-  // logic to play a single round
-  function playRound(humanChoice, compChoice) {
-    let humanchoice = humanChoice.toLowerCase();
-
-    if (humanchoice === compChoice) console.log("Draw!");
-    else if (compChoice === "rock" && humanchoice === "scissor") {
-      compScore++;
-      console.log("Rock smashes Scissor, you lost this round!");
-    } else if (compChoice === "paper" && humanchoice === "rock") {
-      compScore++;
-      console.log("Paper swallows the rock, you lost this round!");
-    } else if (compChoice === "scissor" && humanchoice === "paper") {
-      compScore++;
-      console.log("Scissor cuts paper, you lost this round!");
-    } else {
-      humanScore++;
-      console.log(`you won this round ${humanchoice} beats ${compChoice}`);
-    }
+  if (humanchoice === compChoice) someString = "Draw!";
+  else if (compChoice === "rock" && humanchoice === "scissor") {
+    compScore++;
+    someString = "Rock smashes Scissor, you lost this round!";
+  } else if (compChoice === "paper" && humanchoice === "rock") {
+    compScore++;
+    someString = "Paper swallows the rock, you lost this round!";
+  } else if (compChoice === "scissor" && humanchoice === "paper") {
+    compScore++;
+    someString = "Scissor cuts paper, you lost this round!";
+  } else {
+    humanScore++;
+    someString = `you won this round ${humanchoice} beats ${compChoice}`;
   }
 
-  for (let i = 1; i <= 5; i++) {
-    // the variables will take the return values of the functions
-    let humanSelection = getHumanChoice();
-    let compSelection = getComputerChoice();
+  let txt = document.createTextNode(someString);
+  div.appendChild(txt);
 
-    playRound(humanSelection, compSelection);
+  let finalTxt;
+
+  if (humanScore === 5 && compScore < 5) {
+    finalTxt = document.createTextNode(
+      `You won this game ${humanScore} to ${compScore}!`
+    );
+  } else {
+    finalTxt = document.createTextNode(
+      `You lost this game ${humanScore} to ${compScore}!`
+    );
   }
-
-  if (humanScore > compScore)
-    console.log(`You won this game ${humanScore} to ${compScore}!`);
-  else if (humanScore < compScore)
-    console.log(`You lost this game ${humanScore} to ${compScore}!`);
-  else console.log("DRAW!");
+  div.appendChild(finalTxt);
 }
 
-playGame();
+// the variables will take the return values of the functions
+
+btns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    let humanChoice = btn.getAttribute("id"); // Get player's choice from button ID
+    let compChoice = getComputerChoice(); // Get random computer choice
+    playRound(humanChoice, compChoice); // Play the round
+  });
+});
+
+// Append the text node to the div
